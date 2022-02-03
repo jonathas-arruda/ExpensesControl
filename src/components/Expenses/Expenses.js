5113d9babdd0e9fcb2a9e96604204854daf51aa1
Jonathas Arruda <jonathasarruda@outlook.com>
2022-02-03 11:55:49 -0300

    sharing study project

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import ExpenseIten from "./ExpenseIten";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectYear) => {
+    setFilteredYear(selectYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expenseContent = <p> No Expenses Found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseIten
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenseContent}
+      </Card>
+    </div>
+  );
+};
+export default Expenses;
