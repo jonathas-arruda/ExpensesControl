8691c5e1564a90bd6575e67aa510cfcb5836370a
Jonathas Arruda <jonathasarruda@outlook.com>
2022-02-03 21:18:19 -0300

    Adding changes for the menu of the new expenses

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 import ExpenseDate from "../Expenses/ExpenseDate";
 
 const NewExpense = (props) => {
-  
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
-        ...enteredExpenseData,
-        id: Math.random().toString()
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
-};
+    // setIsEditing(false)
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const endEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={endEditingHandler}
+        />
+      )}
     </div>
   );
 };
